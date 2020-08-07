69c203d8e9e98af079624b3358db505973e68e24
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2020-08-07 13:46:56 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# gh-pages-pelican-action
-Pelican Action for GitHub Pages
+# GitHub Pages Pelican Action
+
+This action builds a Pelican site and deploys it on github pages
+
+Make sure that requirements such as `Gemfile` are up to date.
+
+If there's an existing `build.sh` it will be executed to setup extra steps before 'building' with Pelican the site (additional steps, other external programs, etc)
+
+## Environment variables
+
+- `GH_PAGES_BRANCH` (optional): override the default `gh-pages` deployment branch
+- `SOURCE_FOLDER` (optional): override the default `./` folder for building the site
---
+++ entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+echo "REPO: $GITHUB_REPOSITORY"
+echo "ACTOR: $GITHUB_ACTOR"
+
+echo '=================== Prepare pip ==================='
+umask 0002
+
+[ -f requirements.txt ] && pip install -r requirements.txt
+
+chmod -R u+rwX,go+rwX,go+rwX /github/workspace/
+
+
+if [ -f build.sh ]; then
+    echo '=================== Running extra setup ==================='
+    bash -x build.sh
+fi
+
+echo '=================== Build site ==================='
+pelican ${SOURCE_FOLDER:=content} -o output -s ${PELICAN_CONFIG_FILE:=pelicanconf.py}
+
+echo '=================== Publish to GitHub Pages ==================='
+cd _site
+remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+remote_branch=${GH_PAGES_BRANCH:=gh-pages}
+git init
+git remote add deploy "$remote_repo"
+git checkout $remote_branch || git checkout --orphan $remote_branch
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git add .
+echo -n 'Files to Commit:' && ls -l | wc -l
+timestamp=$(date +%s%3N)
+git commit -m "[ci skip] Automated deployment to GitHub Pages on $timestamp"
+git push deploy $remote_branch --force
+rm -fr .git
+cd ../
+echo '=================== Done  ==================='
